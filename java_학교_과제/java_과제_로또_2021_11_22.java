756d6e090d05f0c9b9f50ff83c243f4d87b5d303
choi su ho <csw020106@naver.com>
2021-11-28 17:51:18 +0900

    자바과제_로또

---
+++ java_학교_과제/java_과제_로또_2021_11_22.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class java_과제_로또_2021_11_22 {		
+	public static void main(String[] args) {		
+			randomRang();		
+	}
+	 static int randomRang()
+	{ 	 Scanner scanner = new Scanner(System.in);
+	     int num = 0; num = scanner.nextInt(); int count = 0 , tmp =0,check = 0;
+		 int [][] arr = new int [num][7];
+		 for(int a = 0; a < num; a++)
+		 { 			 
+			 while(count != 7)
+			 {
+				 tmp = (int)((Math.random()*45)+1);
+				 for(int i = 0; i < count; i++)
+				 {
+					 if(tmp == arr[a][i])
+					 {
+						 check = 1; break;
+					 }
+				 }
+				 if(check == 1)
+				 {
+					 check = 0;continue;
+				 }
+				 arr[a][count] = tmp;			 
+				 count ++;			 
+		 	}
+			 count = 0;
+			 for(int i = 0; i < 7; i++)
+			 {	 if(i !=6)
+				 {System.out.print((i+1)+"번"+arr[a][i]+" ");}
+				 else
+				 {System.out.print("보너스 숫자"+arr[a][i]);}
+			 }
+			 System.out.println();
+			 }
+		scanner.close(); return 0;}
+}
