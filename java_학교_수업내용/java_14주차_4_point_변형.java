c3009478480de51d4f9e28a733579f21f17d24b2
choi su ho <csw020106@naver.com>
2021-12-06 22:02:47 +0900

    상속의 개념과 기본 생성자를 데리고가는 기준(2021/12/06)

--- java_학교_수업내용/java_14주차_4_point_변형.java
+++ java_학교_수업내용/java_14주차_4_point_변형.java
@@ -1,9 +1,9 @@
 public class java_14주차_4_point_변형 {
     private int x,y;
 
-    public java_14주차_4_point_변형()
-    {
-        x = y = 0;
+    public java_14주차_4_point_변형() // 이걸 부모로 가질 클래스에서 
+    {                                // 만약 개발자가 어떤 클래스를 가져올지 지정안한다면
+        x = y = 0;                   // 가장 컴파일러가 기본껄 가져온다
     }
     public java_14주차_4_point_변형(int x,int y)
     {
