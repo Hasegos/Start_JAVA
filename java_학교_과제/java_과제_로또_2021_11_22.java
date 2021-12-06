49cfa6c88125ec1da9503aa2613e29c29e08384e
choi su ho <csw020106@naver.com>
2021-12-06 14:08:59 +0900

    자바과제_로또

--- java_학교_과제/java_과제_로또_2021_11_22.java
+++ java_학교_과제/java_과제_로또_2021_11_22.java
@@ -3,10 +3,11 @@ public class java_과제_로또_2021_11_22 {
 	public static void main(String[] args) {		
 			randomRang();		
 	}
-	 static int randomRang()
-	{ 	 Scanner scanner = new Scanner(System.in);
+	static void randomRang()
+	{ 	
+		 Scanner scanner = new Scanner(System.in);
 	     int num = 0; num = scanner.nextInt(); int count = 0 , tmp =0,check = 0;
-		 int [][] arr = new int [num][7];
+		 int [] arr = new int [7];
 		 for(int a = 0; a < num; a++)
 		 { 			 
 			 while(count != 7)
@@ -14,7 +15,7 @@ public class java_과제_로또_2021_11_22 {
 				 tmp = (int)((Math.random()*45)+1);
 				 for(int i = 0; i < count; i++)
 				 {
-					 if(tmp == arr[a][i])
+					 if(tmp == arr[i])
 					 {
 						 check = 1; break;
 					 }
@@ -23,17 +24,18 @@ public class java_과제_로또_2021_11_22 {
 				 {
 					 check = 0;continue;
 				 }
-				 arr[a][count] = tmp;			 
+				 arr[count] = tmp;			 
 				 count ++;			 
 		 	}
 			 count = 0;
 			 for(int i = 0; i < 7; i++)
 			 {	 if(i !=6)
-				 {System.out.print((i+1)+"번"+arr[a][i]+" ");}
+				 {System.out.print((i+1)+"번"+arr[i]+" ");}
 				 else
-				 {System.out.print("보너스 숫자"+arr[a][i]);}
+				 {System.out.print("보너스 숫자"+arr[i]);}
 			 }
 			 System.out.println();
 			 }
-		scanner.close(); return 0;}
+		scanner.close(); 
+	}
 }
