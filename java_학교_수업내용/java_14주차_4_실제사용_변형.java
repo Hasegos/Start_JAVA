23f7842bc0dbae302d99995b0103597e3e191f1c
choi su ho <csw020106@naver.com>
2021-12-06 22:06:02 +0900

    상속개념을 이용해서 사용해보기(2021/12/06)

---
+++ java_학교_수업내용/java_14주차_4_실제사용_변형.java
@@ -0,0 +1,7 @@
+public class java_14주차_4_실제사용_변형 {
+    
+    public static void main(String[] args) {
+        java_14주차_4_ColorPoint_변형  cp = new java_14주차_4_ColorPoint_변형(100,200,"red");
+        cp.showXYColor();
+    }
+}
