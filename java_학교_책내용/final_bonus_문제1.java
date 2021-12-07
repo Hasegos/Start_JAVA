5ddb4c0da9fc9f7abf8f6d6ee8bf5979c713b93a
choi su ho <csw020106@naver.com>
2021-12-07 19:56:06 +0900

    2단원 마지막문제 Bonus 1

---
+++ java_학교_책내용/final_bonus_문제1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class final_bonus_문제1 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("식을 입력하세요");
+        double op1 = scanner.nextInt();
+        String operator = scanner.next();
+        double op2 = scanner.nextInt();
+        double result = 0;
+        scanner.close();
+        switch(operator)
+        {
+            case "+":result = op1 + op2; break;
+            case "-":result = op1 - op2; break;
+            case "*":result = op1 * op2; break;
+            case "/":
+                if(op2 == 0)
+                {
+                    System.out.println("0 으로 나눌 수 없습니다.");
+                    return;
+                }
+                result = op1 / op2; break;
+       
+        default:System.out.println("연산 기호가 잘못되었습니다.");
+        }
+        System.out.println(result);
+        
+    }
+}
