c762bf75e39db632fc0c63f968a08e417b1d732e
choi su ho <csw020106@naver.com>
2021-12-08 10:26:21 +0900

    for 문을 이용하여 1부터 10까지 합 출력하기_1

---
+++ java_학교_책내용/java_책_3단원/for_문을_이용하여_1부터_10까지_합출력하기.java
@@ -0,0 +1,20 @@
+package java_책_3단원;
+public class for_문을_이용하여_1부터_10까지_합출력하기 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int  j = 1;  j <11; j++)
+        {
+            sum += j;
+            System.out.print(j);
+            if(j <= 9)
+            {
+                System.out.print("+");
+            }
+            else
+            {
+                System.out.print("=");
+                System.out.print(sum);
+            }
+        }
+    }
+}
