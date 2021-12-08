8aed41a9479d2fbdda154b5ba8a9bb3836d2ab3b
choi su ho <csw020106@naver.com>
2021-12-08 10:08:45 +0900

    Hello2030_2

---
+++ java_학교_책내용/java_책_1단원/Hello2030.java
@@ -0,0 +1,12 @@
+package java_책_1단원;
+public class Hello2030 {
+    
+    public static void main(String[] args) {
+        
+           int n=2030;
+           System.out.println("헬로"+n);
+            
+            return;
+        
+    }
+}
