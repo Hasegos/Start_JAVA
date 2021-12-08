0c6ef6026886773f3b98418e0e4cfaaade8a2aa3
choi su ho <csw020106@naver.com>
2021-12-08 10:13:30 +0900

    비교 연산자와 논리 연산자 사용하기_7

---
+++ java_학교_책내용/java_책_2단원/비교_논리_연산자.java
@@ -0,0 +1,18 @@
+package java_책_2단원;
+public class 비교_논리_연산자 {
+    public static void main(String[] args) {
+        System.out.println('a' > 'b');  // false
+        System.out.println(3 >= 2); // true
+        System.out.println(-1 < 0); // true
+        System.out.println(3.45 <= 2); // false
+        System.out.println(3 == 2); // false
+        System.out.println(3 != 2); // true
+        System.out.println(!(3 != 2)); // false
+        System.out.println((3 > 2) && (3 > 4)); // false
+        System.out.println((3 != 2) /* || (-1 > 0) */ ); // true
+        System.out.println((3 != 2) ^ (-1 >0)); // xor 연산자  모두 false 인경우만 false   그러므로 true
+
+        return;
+        
+    }
+}
