57b7cc4adf74d532fb0ade4ba40f14169bb41fc1
choi su ho <csw020106@naver.com>
2021-12-08 09:31:46 +0900

    나머지를 생각하자

---
+++ java_학교_책내용/실습문제_3단원_2번문제.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class 실습문제_3단원_2번문제 {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new  Scanner(System.in);
+        int[] num = new int[11];
+        System.out.print("정수 10개 입력>>");
+        for(int i = 0 ; i < 10; i++)
+        {
+            num[i] = scanner.nextInt();
+        }
+        for(int i = 0 ; i < 10; i++)
+        {
+            if(num[i] % 3 == 0)
+            {
+                System.out.print(num[i]+" ");
+            }
+        }
+        scanner.close();
+    }
+}
