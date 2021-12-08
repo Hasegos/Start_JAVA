2b23ef436029efd0779f0026c7e212c7fbd64b3f
choi su ho <csw020106@naver.com>
2021-12-08 10:31:31 +0900

    for-each 문 활용_9

---
+++ java_학교_책내용/java_책_3단원/for_each_문활용.java
@@ -0,0 +1,19 @@
+package java_책_3단원;
+public class for_each_문활용 {
+    public static void main(String[] args) {
+        
+        int[] n = {1,2,3,4,5};
+        int sum = 0;
+        for(int k : n) // k 에다가 n[0]  , n[1]  , n [4]로 반복
+        {
+            System.out.print(k+" ");
+            sum += k;
+        }
+        System.out.println("합은 "+sum);
+        String f[] = {"사과","배","바나나","체리","딸기","포도"};
+        for(String k : f)
+        {
+            System.out.print(k + " ");
+        }
+    }
+}
