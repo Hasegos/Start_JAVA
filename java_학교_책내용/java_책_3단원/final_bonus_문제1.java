19bb4ec7fa647cd955b531a42fded59c14ed36d6
choi su ho <csw020106@naver.com>
2021-12-10 21:04:21 +0900

    3단원 마지막 2차원 배열과 반복문 연습_23

---
+++ java_학교_책내용/java_책_3단원/final_bonus_문제1.java
@@ -0,0 +1,36 @@
+package java_책_3단원;
+
+public class final_bonus_문제1 {
+    
+    public static void main(String[] args) {
+        
+        int intArray[][];
+        intArray =  new int[3][4];
+
+        for(int i = 0 ; i < 3; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                intArray[i][j] = (int)(Math.random()*10);
+            }
+        }
+        for(int i = 0 ; i < intArray.length; i++)
+        {
+            for(int j = 0; j < intArray[i].length ; j++)
+            {
+                System.out.print(intArray[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        int i = 0 , sum = 0;
+        while(i < 3)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                sum += intArray[i][j];
+            }
+            i++;
+        }
+        System.out.println("합은 " +sum);
+    }
+}
