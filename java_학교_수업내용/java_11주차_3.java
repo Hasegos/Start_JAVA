13bf556b8864dd2d424811b838e22be8d71449ef
choi su ho <csw020106@naver.com>
2021-12-13 18:36:00 +0900

    java_11주차_3번내용(2021/11/16)

--- java_학교_수업내용/java_11주차_3.java
+++ java_학교_수업내용/java_11주차_3.java
@@ -1,7 +1,8 @@
 import java.util.*; // 입력을 위한 키보드 생성  *: 자바에 있는 모든 라이브러리 가져오기
 // 학교 수업내용에서 가장 큰 오점은 한국어 입력시 제대로 작동이안된다는것이다.
 
-public class java_11주차_3 {
+public class java_11주차_3
+{
     static ArrayList<String> vCityName = new ArrayList<String>(); //viCityName 에 새로운 문자열 입력받는곳 생성
     static Scanner scanner =  new Scanner(System.in); // 입력 받는 곳 scanner 
                                                // 그리고 함수 모든 곳에서 입력받는다면 main 함수안이 아닌
@@ -26,9 +27,7 @@ public class java_11주차_3 {
                 vCityName.add(cityName); // 나머지는 다 새로운것이니 추가해준다
             }
         }
-    }
-    
-    
+    }   
     static int checkDup(String ciName) // checkDup 합수
     {
         for(String temp:vCityName) // 문자열 입력받은 곳에서 하나하나 temp 로 옮겨서 비교
@@ -37,8 +36,6 @@ public class java_11주차_3 {
         return 1; // 아니면 1반환
 
     }
-    
-
     static void searchData()  // search 함수
     {   
         String cName; // 문자 cName 선언 
@@ -52,9 +49,7 @@ public class java_11주차_3 {
         {
             System.out.println("없네요");
         }
-
     }
-
     static void  printList() // printList 함수 출력 함수
     {
         System.out.println("방문했던 도시이름목록:");
@@ -62,36 +57,28 @@ public class java_11주차_3 {
         {
             System.out.print(tmep+" ");// 그 보낸걸출력하기
         }
-
     }
-
     public static void main(String[] args) {  // main 함수
 
         String menu;         
-        System.out.println("방문했던 도시 이름 관리 프로그램");
-
-    
-    do
-    {
-        System.out.print("1. 입력  2. 검색  3. 출력  4.종료....");
-        menu = scanner.nextLine();
-        switch (menu)  // 숫자 같은경우는 enter 이걸 다 가져가기때문에 문제가 발생했었다 
-        {              // 그래서 문자로 하면 enter를 버리기때문에 문제를 해결했었다.
-                       // 또한 enter space  등 whitespace라고한다.
-            case "1":inData();  // 입력을 위해 inData함수 호출
-               break;
-            case "2":searchData(); //  검색을 위해 searchData함수 호출
-               break;
-            case "3":printList(); //  출력을 위해 printList함수 호출
-               break;
-            case "4":System.exit(0); // 시스템 종료
-            default:System.out.println("메뉴 선택이 잘못됨");
-            
-        }
-    }while(true);
-        
-
-
-    }
-   
+        System.out.println("방문했던 도시 이름 관리 프로그램");    
+        do
+        {
+            System.out.print("1. 입력  2. 검색  3. 출력  4.종료....");
+            menu = scanner.nextLine();
+            switch (menu)  // 숫자 같은경우는 enter 이걸 다 가져가기때문에 문제가 발생했었다 
+            {              // 그래서 문자로 하면 enter를 버리기때문에 문제를 해결했었다.
+                        // 또한 enter space  등 whitespace라고한다.
+                case "1":inData();  // 입력을 위해 inData함수 호출
+                break;
+                case "2":searchData(); //  검색을 위해 searchData함수 호출
+                break;
+                case "3":printList(); //  출력을 위해 printList함수 호출
+                break;
+                case "4":System.exit(0); // 시스템 종료
+                default:System.out.println("메뉴 선택이 잘못됨");
+                
+            }
+        }while(true);
+    }    
 }
