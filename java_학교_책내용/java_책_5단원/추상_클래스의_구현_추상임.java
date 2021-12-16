59288611780c2e4eb52c05fa139d68e89bde42c8
choi su ho <csw020106@naver.com>
2021-12-17 00:57:20 +0900

    추상 클래스의 구현의 추상_6

---
+++ java_학교_책내용/java_책_5단원/추상_클래스의_구현_추상임.java
@@ -0,0 +1,7 @@
+package java_책_5단원;
+
+abstract class 추상_클래스의_구현_추상임 {
+    public abstract int add (int a, int b);
+    public abstract int subtract(int a, int b);
+    public abstract double average(int[] a);
+}
